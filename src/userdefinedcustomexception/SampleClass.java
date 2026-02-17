0fec4c6a7ee57e68451744436dc984b9f7812c41
Chandan K M <chandanchintu16@gmail.com>
2026-02-17 22:07:48 +0530

    exception handling examples

--- src/userdefinedcustomexception/SampleClass.java
+++ src/userdefinedcustomexception/SampleClass.java
@@ -4,6 +4,7 @@ public class SampleClass {
     public static void main(String[] args) {
         try {
             String user = null;
+
             if(user==null){
                 throw  new UserNotFoundException("user is not present!!");
             }
